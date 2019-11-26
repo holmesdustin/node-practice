e17135dd68570b17aae76bae6e1273a50101bf6c
Yujing Gao <gao.yujing.csu@gmail.com>
2019-11-25 22:41:11 -0500

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+const express = require('express');
+var app = express();
+var port = process.env.PORT | 3000;
+
+app.set('views', path.join(__dirname, "htmls"));
+app.set('view engine', 'ejs');
+app.use(express.static('public')); //Express serves images, CSS files, and JavaScript files in a directory named public
+app.use(bodyParser.urlencoded({encoded: true}));
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+http.createServer(app).listen(port, function(){
+
+});
